c69fb0a625b52d52b4c6d2adc4ba31f5fea867da
Tim Sassen <t.sassen@xsarus.nl>
2024-04-14 11:24:08 +0200

    ascontroller attribute

--- Controller/CookieConsentController.php
+++ Controller/CookieConsentController.php
@@ -15,11 +15,13 @@ use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Twig\Environment;
 
+#[AsController]
 class CookieConsentController
 {
     /**
